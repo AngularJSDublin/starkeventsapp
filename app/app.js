ff9defb86ba3c750cd1cc5f8dfb21931c87723b6
Eoin Ó Loideáin <eoloideain@gmail.com>
2015-06-13 14:28:29 +0100

    add routes

--- app/app.js
+++ app/app.js
@@ -1,7 +1,12 @@
 'use strict';
 
 (function(){
-	var app = angular.module('eventsApp', []);
+	var app = angular.module('eventsApp', ['ngRoute']);
+
+	angular
+		.module('eventsApp')
+		 .config(router);
+
 
 	//controller as syntax
 	app.controller('SampleController', function(){
@@ -11,5 +16,24 @@
 	app.controller("SecondController", function($scope){
 		$scope.welcomeAgain = 'By the end of the workshops you will build the Angular events app and hopefully understand the framework on deeper level.'
 	})
+
+//creating the routes 
+	function router ($routeProvider, $locationProvider) {
+		  // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+
+		$routeProvider
+		  .when('/add-event', {
+		  	templateUrl: 'components/add-event/add-event.html'
+		  })
+		  .when('/event-list', {
+		  	templateUrl: 'components/event-list/event-list.html'
+		  })
+		  .otherwise({
+		    redirectTo: '/'
+		  });
+
+		
+	}
 
 })()
