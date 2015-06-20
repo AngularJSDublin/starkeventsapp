cd6e56efeb4a9013b62cb407c8f42c97129af853
hubkic <hubkic@gmail.com>
2015-06-20 10:13:44 +0100

    Corrected add-event page

--- app/components/add-event/add-event.js
+++ app/components/add-event/add-event.js
@@ -6,10 +6,8 @@ angular.module('eventsApp.addEvent')
 			imageUrl:"something",
 			longitude:"121",
 			latitude:"3423",
-			startDate: new Date("July 21, 2015"),
-			startTime: new Date("July 21, 2015 13:50"),
-			endDate: new Date("July 21, 2015"),
-			endTime: new Date("July 21, 2015 13:50"),
+			startDate: new Date("July 21, 2015 13:50"),
+			endDate: new Date("July 21, 2015 13:50")
 		}
 
 		$scope.addEvent = function(){
