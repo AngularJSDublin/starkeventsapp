4348d5a6023d4a8eefab7abb338dda5167604d98
hubkic <hubkic@gmail.com>
2015-06-20 10:45:42 +0100

    merge

--- app/components/add-event/add-event.js
+++ app/components/add-event/add-event.js
@@ -1,13 +1,17 @@
 angular.module('eventsApp.addEvent')
 .controller("addEventController", function($scope){
 		$scope.event = {
-			title:"Title",
-			description:"Description",
-			imageUrl:"something",
-			longitude:"121",
-			latitude:"3423",
-			startDate: new Date("July 21, 2015 13:50"),
-			endDate: new Date("July 21, 2015 13:50")
+			"name": "Title",
+			"description": "Description",
+			"date": new Date("July 21, 2015 13:50"),
+			"location": {
+	          "lat": 53.3168242,
+	          "lng": -6.2015825
+	      	},
+			"allSpots": 12000,
+	      	"availableSpots": 3423,
+			"image_url":"https://static.pexels.com/photos/2361/nature-animal-wolf-wilderness.jpg"
+
 		}
 
 		$scope.addEvent = function(){
