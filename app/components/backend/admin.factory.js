836320d94c4954bdde1660d135da8c5ab4be8075
Pencroff <pencroff@gmail.com>
2015-06-27 15:46:52 +0100

    Admin management - list and create

--- app/components/backend/admin.factory.js
+++ app/components/backend/admin.factory.js
@@ -35,6 +35,7 @@
 
         function getById(id) {
             var url = 'https://starkeventsdb.firebaseio.com/admins/' + id + '.json';
+            url += '?auth=' +  auth.getToken();
             return $http
                 .get(url)
                 .then(getByIdComplete)
@@ -50,6 +51,7 @@
 
         function deleteById(id) {
             var url = 'https://starkeventsdb.firebaseio.com/admins/' + id + '.json';
+            url += '?auth=' +  auth.getToken();
             return $http
                 .delete(url)
                 .then(deleteByIdComplete)
@@ -63,20 +65,36 @@
             }
         }
 
-        function add(eventObj) {
+
+        /**
+         *
+         * @param adminObj [object]
+         * {
+         *  email: 'mail@mail.com',
+         *  name: 'some admin name',
+         *  age: some integer value,
+         *  password: 'future password for new admin'
+         * }
+         * @returns {*} - promise
+         */
+        function add(adminObj) {
             // bad approach, should use other id declaration
-            return getList()
+            var data = processAdminObj(adminObj);
+            return auth.create(data.login, data.password)
+                .then(function() {
+                    return getList();
+                })
                 .then(function (data) {
                     var id = 'user-' + data.length;
-                    return edit(id, eventObj);
+                    return edit(id, adminObj);
                 });
         }
 
-        function edit(id, eventObj) {
+        function edit(id, adminObj) {
             var url = 'https://starkeventsdb.firebaseio.com/admins/' + id + '.json';
-
+            url += '?auth=' +  auth.getToken();
             return $http
-                .put(url, eventObj)
+                .put(url, adminObj)
                 .then(editComplete)
                 .catch(editFailed);
 
@@ -86,6 +104,19 @@
             function editFailed(error) {
                 $log.error('XHR Failed for EventService add / edit: ' + JSON.stringify(error.data, null, 2));
             }
+        }
+
+        function processAdminObj(adminObj){
+            var result = {
+                login: adminObj.email,
+                password: adminObj.password,
+                data: {
+                    email: adminObj.email,
+                    name: adminObj.name,
+                    age: adminObj.age
+                }
+            };
+            return result;
         }
     }
 })(angular);
